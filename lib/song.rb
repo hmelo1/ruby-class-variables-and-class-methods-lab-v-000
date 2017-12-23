4e000102b5b744b175feda54c45ee6c547854dbd
Eddie Melo <heriberto@melo.nyc>
2017-12-23 03:20:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,6 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@count = 0
+  def new=(name, artist, genre)
 
-  def initialize(name)
-    @name = name
-    @@count += 1
-    @@genres = {}
-  end
-
-  def self.count
-    @@count
-  end
-
-  def genres
-    @@genres.uniq
-  end
-
-  def new(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
   end
 end
